d8101ee58e928cf98171a73065ac7aff1556f947
sunya <ydsunyan123@gmail.com>
2025-02-24 15:42:21 +0800

    feat: better vertical window border.

--- lisp/init-interface.el
+++ lisp/init-interface.el
@@ -7,5 +7,12 @@
 (require 'awesome-tab)
 (awesome-tab-mode t)
 
+;; Better window border
+(set-face-background 'vertical-border (face-background 'default))
+(set-face-foreground 'vertical-border "white")
+(set-display-table-slot standard-display-table
+                        'vertical-border
+                        (make-glyph-code ?┃))
+
 (provide 'init-interface)
 ;;; init-utils.el ends here
