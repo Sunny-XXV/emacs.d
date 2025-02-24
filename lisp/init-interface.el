383bd39fafe69876e1d41238a3f001d9fced5028
sunya <ydsunyan123@gmail.com>
2025-02-24 18:17:16 +0800

    fix: better interface.

--- lisp/init-interface.el
+++ lisp/init-interface.el
@@ -6,6 +6,11 @@
 
 (require 'awesome-tab)
 (awesome-tab-mode t)
+(setq-default awesome-tab-background-color (face-background 'default))
+(setq awesome-tab-ace-str-style 'left)
+(when (not (display-graphic-p))
+  (setq frame-background-mode 'dark))
+
 
 ;; Better window border
 (set-face-background 'vertical-border (face-background 'default))
@@ -13,6 +18,13 @@
 (set-display-table-slot standard-display-table
                         'vertical-border
                         (make-glyph-code ?┃))
+
+(require 'neotree)
+(setq neo-theme 'nerd-icons)
+(add-hook 'neotree-mode-hook
+          (lambda ()
+            (display-line-numbers-mode -1)
+            (setq mode-line-format nil)))
 
 (provide 'init-interface)
 ;;; init-utils.el ends here
