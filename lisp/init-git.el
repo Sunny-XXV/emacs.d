7903050af06a7ffbbe180bb702caab2d8d3454d7
sunya <ydsunyan123@gmail.com>
2025-02-24 11:37:13 +0800

    feat: add awesome-tray and hide modeline.

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -106,7 +106,7 @@
 (require 'auto-save)
 (auto-save-enable)
 (setq auto-save-silent t)   ; quietly save
-(setq auto-save-delete-trailing-whitespace t)  ; automatically delete spaces at the end of the line when saving
+(setq auto-save-delete-trailing-whitespace nil)  ; automatically delete spaces at the end of the line when saving
 
 (provide 'init-git)
 ;;; init-git.el ends here
